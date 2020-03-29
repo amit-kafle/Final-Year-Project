f0dcbb32f4b6449adf5a74ad488aa35de2a9fbac
Amit Kafle <amitkafle1@gmail.com>
2020-03-30 00:58:23 +0545

    Creating registration form and connecting two activities

--- app/src/main/java/com/amitkafle/phonebulance/LoginActivity.java
+++ app/src/main/java/com/amitkafle/phonebulance/LoginActivity.java
@@ -7,20 +7,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 EditText phone,password;
 Button signin;
+TextView signup;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         phone = findViewById(R.id.phone);
         password = findViewById(R.id.password);
         signin = findViewById(R.id.signin);
+        signup=findViewById(R.id.signupa);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,SignupActivity.class));
+            }
+        });
+
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+
 
             }
         });
